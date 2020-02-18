210071063766da581814c1d15c3daa94fc4dbc3c
Patrick <patr6996@edu.easj.dk>
2020-02-18 12:46:17 +0100

    Lommeregner, med funktionerne. ADD, SUBTRACT, MULTIPLY AND DIVISION.

---
+++ app/src/main/java/com/example/lommeregner/Lommeregner.java
@@ -0,0 +1,21 @@
+package com.example.lommeregner;
+
+public class Lommeregner {
+
+    public double add(double a, double b) {
+     return a + b;
+
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+}
---
+++ app/src/main/java/com/example/lommeregner/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.lommeregner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Lommeregner lommeregner = new Lommeregner();
+
+    private EditText doubleA;
+    private TextView textA;
+
+    private EditText doubleB;
+    private TextView textB;
+
+    private EditText resultat;
+    private TextView resultatText;
+
+    private Button addButton;
+    private Button subtractButton;
+    private Button multiplyButton;
+    private Button divisionButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        doubleA = findViewById(R.id.doubleA);
+        textA = findViewById(R.id.textA);
+
+        doubleB = findViewById(R.id.doubleB);
+        textB = findViewById(R.id.textB);
+
+        resultat = findViewById(R.id.resultat);
+        resultatText = findViewById(R.id.resultatText);
+
+        addButton = findViewById(R.id.addButton);
+        subtractButton = findViewById(R.id.subtractButton);
+        multiplyButton = findViewById(R.id.multiplyButton);
+        divisionButton = findViewById(R.id.divisionButton);
+    }
+
+    public void addClick(View view) {
+
+        try {
+            double a = Double.parseDouble(doubleA.getText().toString());
+            double b = Double.parseDouble(doubleB.getText().toString());
+
+            resultat.setText(Double.toString(lommeregner.add(a, b)));
+
+        } catch (Exception e) {
+            resultat.setText("Illegal input, try again");
+        }
+    }
+
+    public void subtractClick(View view) {
+        try {
+            double a = Double.parseDouble(doubleA.getText().toString());
+            double b = Double.parseDouble(doubleB.getText().toString());
+
+            resultat.setText(Double.toString(lommeregner.subtract(a, b)));
+
+        } catch (Exception e) {
+            resultat.setText("Illegal input, try again");
+        }
+    }
+
+    public void multiplyClick(View view) {
+        try {
+            double a = Double.parseDouble(doubleA.getText().toString());
+            double b = Double.parseDouble(doubleB.getText().toString());
+
+            resultat.setText(Double.toString(lommeregner.multiply(a, b)));
+
+        } catch (Exception e) {
+            resultat.setText("Illegal input, try again");
+        }
+    }
+
+    public void divisionClick(View view) {
+
+
+        try {
+            double a = Double.parseDouble(doubleA.getText().toString());
+            double b = Double.parseDouble(doubleB.getText().toString());
+
+            resultat.setText(Double.toString(lommeregner.division(a, b)));
+
+        } catch (Exception e) {
+            resultat.setText("Illegal input, try again");
+        }
+    }
+
+
+
+
+
+}
